bc15e68f14f25740c7f42602ba77991787151450
samikshakulkarni <kulkarni.d.samiksha@gmail.com>
2021-05-13 12:24:01 +0530

    Binary search Iterative

--- binsearch.cpp
+++ binsearch.cpp
@@ -1,6 +1,39 @@
 #include<iostream>
 using namespace std;
+int binsearch(int arr[], int  l,int r, int target)
+{
+    int mid;
+    while(l<=r)
+    {
+        mid = (l+r)/2;
+        if(arr[mid] == target)
+            return mid;
+        if(arr[mid] > target)
+            r = mid - 1;
+        else 
+            l = mid+1;
+    }
+
+    return -1; // if not found 
+}
 int main()
 {
+int n; cout << "enter n" << endl;
+    cin >> n;
+    int a[n];
+    cout << "enter array" << endl;
+    int x;
+    cout << "enter element to be searched" << endl;
+    cin >> x;
 
+    for(int i=0;i<n;i++)
+    {
+        cin >> a[i];
+    }
+    sort(a,a+10);
+    if(binsearch(a,0,n,x) == -1)
+    {
+        cout << "Element not present" << endl;
+    }
+    else cout << "element present at" << binsearch(a,0,n,x) << endl;
 }
